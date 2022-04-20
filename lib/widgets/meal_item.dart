5e9418fdf40cd3123e3f043a644c93d2887262d8
hyojinlee <lhj910129@gmail.com>
2022-04-20 16:30:49 +0900

    시간, 난이도, 가격 enum을 텍스트로 가져오는 Get추가

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -17,6 +17,47 @@ class MealItem extends StatelessWidget {
     required this.affordability,
   }) : super(key: key);
 
+  //난이도를 텍스트로 가져오기 위한 겟
+  String get complexityText {
+    String result;
+
+    switch (complexity) {
+      case Complexity.Simple:
+        result = 'Simple';
+        break;
+      case Complexity.Challenging:
+        result = 'Challenging';
+        break;
+      case Complexity.Hard:
+        result = 'Hard';
+        break;
+      default:
+        result = 'Unknown';
+    }
+    return result;
+  }
+
+  //가격을 텍스트로 가져오기 위한 겟
+  String get affordabilityText {
+    String result;
+
+    switch (affordability) {
+      case Affordability.Affordable:
+        result = 'Affordable';
+        break;
+      case Affordability.Luxurious:
+        result = 'Luxurious';
+        break;
+      case Affordability.Pricey:
+        result = 'Pricey';
+        break;
+      default:
+        result = 'Unknown';
+    }
+    return result;
+  }
+
+
   void selectMeal() {}
 
   @override
@@ -45,7 +86,64 @@ class MealItem extends StatelessWidget {
                     fit: BoxFit.cover,
                   ),
                 ),
+                Positioned(
+                  /*위치를 지정할 수 있는 위젯*/
+                  bottom: 20,
+                  right: 10,
+                  child: Container(
+                    width: 300,
+                    color: Colors.black54,
+                    padding: EdgeInsets.symmetric(
+                      vertical: 5,
+                      horizontal: 20,
+                    ),
+                    child: Text(
+                      title,
+                      style: TextStyle(
+                        fontSize: 26,
+                        color: Colors.white,
+                      ),
+                      softWrap: true /*텍스트 래핑*/,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
+                ),
               ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround, //가로 위치를 균등하게 맞춤
+                children: [
+                  Row(
+                    children: [
+                      Icon(Icons.schedule),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text('$duration min'),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.work),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text(complexityText),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.attach_money),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text(affordabilityText),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
