5fef1bcfd0835fd3cc58abd0549b07d4aa6e3b5f
hyojinlee <lhj910129@gmail.com>
2022-04-20 14:46:41 +0900

    Navigator.of(ctx).pushNamed 를 사용하여 상세페이지로 이동하기

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -10,15 +10,23 @@ class CategoryItem extends StatelessWidget {
   CategoryItem(this.id, this.title, this.color);
 
   void selectCategory(BuildContext ctx) {
-    //Navigator : 다른화면으로 이동하게 해주는 플러터 내장
-    Navigator.of(ctx).push(
-      MaterialPageRoute(
-        //쿠퍼티노페이지루트를 써도 무방
-        builder: (_) {
-          return CategoryMealsScreen(id, title);
-        },
-      ),
-    ); //새로운 페이지로 푸시하겠다는 의미
+    // //Navigator : 다른화면으로 이동하게 해주는 플러터 내장
+    // Navigator.of(ctx).push(
+    //   MaterialPageRoute(
+    //     //쿠퍼티노페이지루트를 써도 무방
+    //     builder: (_) {
+    //       return CategoryMealsScreen(id, title);
+    //     },
+    //   ),
+    // ); //새로운 페이지로 푸시하겠다는 의미
+
+    Navigator.of(ctx).pushNamed(
+      '/category-meals',
+      arguments: {
+        'id': id,
+        'title': title,
+      },
+    );
   }
 
   @override
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './category_meals_screen.dart';
 import './categories_screen.dart';
 
 void main() => runApp(const MyApp());
@@ -30,6 +31,7 @@ class MyApp extends StatelessWidget {
             ),
       ),
       home: CategoriesScreen(),
+      routes: {'/category-meals': (ctx) => CategoryMealsScreen();},
     );
   }
 }
