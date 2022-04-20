02a1e0aba3b6310b27be82feaca1e7c48a610435
hyojinlee <lhj910129@gmail.com>
2022-04-20 13:46:27 +0900

    메인테마 셋팅, Font추가

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'DeliMeal',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyLarge: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              bodyMedium: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              titleMedium: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
       ),
       home: CategoriesScreen(),
     );
