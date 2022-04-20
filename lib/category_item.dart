68302e13da5dd6092efd88aa42c36361c6d8bbfa
hyojinlee <lhj910129@gmail.com>
2022-04-20 14:27:27 +0900

    카테고리명 상세페이지로 넘기기

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -3,10 +3,11 @@ import 'package:flutter/material.dart';
 import 'category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
 
-  CategoryItem(this.title, this.color);
+  CategoryItem(this.id, this.title, this.color);
 
   void selectCategory(BuildContext ctx) {
     //Navigator : 다른화면으로 이동하게 해주는 플러터 내장
@@ -14,7 +15,7 @@ class CategoryItem extends StatelessWidget {
       MaterialPageRoute(
         //쿠퍼티노페이지루트를 써도 무방
         builder: (_) {
-          return CategoryMealsScreen();
+          return CategoryMealsScreen(id, title);
         },
       ),
     ); //새로운 페이지로 푸시하겠다는 의미
--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
-  const CategoryMealsScreen({Key? key}) : super(key: key);
+  final String categoryId;
+  final String categoryTitle;
+
+  CategoryMealsScreen(
+    this.categoryId,
+    this.categoryTitle,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          '레시피',
+          categoryTitle,
         ),
       ),
       body: Container(
