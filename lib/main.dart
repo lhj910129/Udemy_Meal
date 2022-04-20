d28c25536e7c44f6873c7f5b6757563df2d2780d
hyojinlee <lhj910129@gmail.com>
2022-04-20 15:31:55 +0900

    폴더구조 변경

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import './category_meals_screen.dart';
-import './categories_screen.dart';
+import 'screen/category_meals_screen.dart';
+import 'screen/categories_screen.dart';
 
 void main() => runApp(const MyApp());
 
