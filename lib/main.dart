33fc64c7b4a77c09f2e595018926a26fb03c8b3c
hyojinlee <lhj910129@gmail.com>
2022-04-25 18:59:15 +0900

    main.dart를 StateFul위젯으로 변경

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,15 @@ import 'screen/categories_screen.dart';
 
 void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
